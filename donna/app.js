beaf35ac4a0d766d5ba9c8cc669be492e9854587
JackieSL1 <jackie@smallcity.ca>
2022-06-25 21:29:15 -0400

    building whole website

---
+++ donna/app.js
@@ -0,0 +1,38 @@
+const donnaPicture = document.getElementById('donna')
+const leftButton = document.querySelector('.left')
+const rightButton = document.querySelector('.right')
+const donnaDescription = document.querySelector('.description')
+const pictures = ['donna1.jpg', 'donna2.jpg', 'donna3.jpg', 'donna4.jpg']
+const descriptions = [
+    'Donna loves going on walks', 
+    'Donna is a ferocious girl',
+    'Donna loves to watch movies (her favourite movie is Joker (2019)',
+    'Donna HATES wearing hats'
+]
+
+let currentPicture = 0
+
+leftButton.addEventListener('click', changePicture)
+rightButton.addEventListener('click', changePicture)
+
+function changePicture() {
+    if (this.className == 'right') {
+        if (currentPicture + 1 < pictures.length) {
+            currentPicture++
+        }
+        else {
+            currentPicture = 0
+        }
+    }
+    else if (this.className == 'left') {
+        if (currentPicture - 1 >= 0) {
+            currentPicture--
+        }
+        else {
+            currentPicture = pictures.length - 1
+        }
+    }
+    donnaPicture.src = pictures[currentPicture]
+    donnaDescription.innerHTML = descriptions[currentPicture]
+
+}
